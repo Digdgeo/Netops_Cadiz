3f421090e287829ae4b41d650b06043685c14c13
Digdgeo <digd.geografo@gmail.com>
2024-06-23 23:44:01 +0200

    starting to build it

---
+++ setup.py
@@ -0,0 +1,33 @@
+import os
+from setuptools import setup, find_packages
+
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+classifiers = [
+	'Intended Audience :: Proximal Sensing & Remote Sensing Technicians',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3.10',
+    'Programming Language :: Python :: 3.11',
+    'Programming Language :: Python :: 3.12',
+    'Operating System :: OS Independent'
+]
+
+setup(
+	name='netops_cadiz',
+	version='0.1.0',
+	description='Python package to retrieve spectral values for satellite bands from Field Spectroradiometers data',
+	long_description=long_description,
+	url='https://github.com/Digdgeo/Netops_Cadiz',
+	python_requires='>=3.8',
+	author='Diego Garcia Diaz, María Dolores Raya-Sereno',
+	author_email='digd.geografo@gmail.com, mdolores.raya@cchs.csic.es',
+	license='MIT',
+	install_requires=['specdal >= 0.2.1', 'openpyxl >= 3.1.4'],
+	packages=find_packages(include=['netops_cadiz', 'netops_cadiz.*']),
+	zip_safe=False
+)
