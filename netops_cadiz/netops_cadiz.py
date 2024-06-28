9946d6f2633e155b31986bb79948a139d8826174
Digdgeo <digd.geografo@gmail.com>
2024-06-28 20:34:37 +0200

    Adding NDI Calc method and comments for the whole code

--- netops_cadiz/netops_cadiz.py
+++ netops_cadiz/netops_cadiz.py
@@ -20,7 +20,8 @@ class asd():
         self.spec_path = spec_path
         self.sats = {  'S2A': ['MSI', 1, 'blue'],'S2B': ['MSI', 2, 'green'],'L8': ['OLI', 3, 'red'],'L9': ['OLI', 4, 'purple'],'L7': ['ETM+', 7, 'orange'],'L5': ['TM', 6, 'brown'], 'L4': ['TM', 5, 'pink']}
         self.sensors = {'MSI': {"B1": [np.arange(412, 457), 443, 'Coastal blue'], "B2": [np.arange(456, 534), 490, 'Blue'], "B3": [np.arange(538, 584), 560, 'Green'], "B4": [np.arange(646, 685), 665, 'Red'], "B5": [np.arange(695, 715), 705, 'Red edge 1'], "B6": [np.arange(731, 760), 740, 'Red edge 2'], "B7": [np.arange(769, 798), 783, 'Red edge 3'], "B8": [np.arange(760, 908), 842, 'Nir'], "B8A": [np.arange(837, 882), 865, 'Nir 8A'], "B9": [np.arange(932, 959), 945, 'Water vapour'], "B10": [np.arange(1337, 1413), 1375, 'Cirrus'], "B11": [np.arange(1539, 1683), 1610, 'Swir 1'], "B12": [np.arange(2078, 2321), 2190, 'Swir 2']},
-                        'OLI': {"B1": [np.arange(435, 451), 443, 'Coastal blue'], "B2": [np.arange(452, 512), 482, 'Blue'], "B3": [np.arange(533, 590), 562, 'Green'], "B8": [np.arange(503, 676), 590, 'Pan'], "B4": [np.arange(636, 673), 655, 'Red'], "B5": [np.arange(851, 879), 865, 'Nir'], "B9": [np.arange(1363, 1384), 1374, 'Cirrus'], "B6": [np.arange(1566, 1651), 1609, 'Swir 1'], "B7": [np.arange(2107, 2294), 2200, 'Swir 2']},
+                        'OLI': {"B1": [np.arange(435, 451), 443, 'Coastal blue'], "B2": [np.arange(452, 512), 482, 'Blue'], "B3": [np.arange(533, 590), 562, 'Green'], 
+                                "B8": [np.arange(503, 676), 590, 'Pan'], "B4": [np.arange(636, 673), 655, 'Red'], "B5": [np.arange(851, 879), 865, 'Nir'], "B9": [np.arange(1363, 1384), 1374, 'Cirrus'], "B6": [np.arange(1566, 1651), 1609, 'Swir 1'], "B7": [np.arange(2107, 2294), 2200, 'Swir 2']},
                         'ETM+': {"B1": [np.arange(441, 514), 478, 'Blue'], "B2": [np.arange(519, 601), 560, 'Green'], "B3": [np.arange(631, 692), 662, 'Red'], "B4": [np.arange(772, 898), 835, 'Nir'], "B5": [np.arange(1547, 1749), 1648, 'Swir 1'], "B7": [np.arange(2064, 2345), 2205, 'Swir 2']},
                         'TM': {"B1": [np.arange(441, 514), 478, 'Blue'], "B2": [np.arange(519, 601), 560, 'Green'], "B3": [np.arange(631, 692), 662, 'Red'], "B4": [np.arange(772, 898), 835, 'Nir'], "B5": [np.arange(1547, 1749), 1648, 'Swir 1'], "B7": [np.arange(2080, 2345), 2205, 'Swir 2']}}
         
@@ -50,22 +51,20 @@ class asd():
         specs = [os.path.join(self.spec_path, i.strip(pref)) for i in os.listdir(self.spec_path)]
         return specs 
 
-    def spec2sat(self, spectra, name=None, plot=True, save_csv=False, csv_path=None, print_values=True):
-        
-        """Main method that makes most of the work
-
-        Args:
-            spectra (File): asd or txt file with the spectrum reflectance values
-            name (String, optional): name desired for the spectrum ("vegetation", "soil", ...). Defaults to None.
-            plot (bool, optional): Select if you want to plot your data. Defaults to True.
-            save_csv (bool, optional): Select if you want to save the expected satellite bands values to csv. Defaults to False.
-            csv_path (Path, optional): Path to store the csv file. Defaults to None.
-            print_values (bool, optional): Select if you want to print the band values. Defaults to True.
-
-        Returns:
-            DataFrame: DataFrame with the expected satellite bands values
+    def spec2sat(self, spectra, name=None, plot=False, save_csv=False, csv_path=None, print_values=False):
         """
-
+        Procesa el espectro para obtener los valores de las bandas especificadas en los sensores satelitales.
+        Args:
+            spectra (File): archivo .asd o .txt con los valores del espectro.
+            name (String, optional): nombre deseado para el espectro ("vegetation", "soil", ...). Defaults to None.
+            plot (bool, optional): Selecciona si deseas graficar tus datos. Defaults to True.
+            save_csv (bool, optional): Selecciona si deseas guardar los valores de las bandas satelitales esperadas en un csv. Defaults to False.
+            csv_path (Path, optional): Ruta para guardar el archivo csv. Defaults to None.
+            print_values (bool, optional): Selecciona si deseas imprimir los valores de las bandas. Defaults to True.
+        Returns:
+            DataFrame: DataFrame con los valores esperados de las bandas satelitales.
+        """
+        # Reading data using pandas when input is a txt file and Specdal in case input is a asd file
         if spectra.endswith('.txt'):
             try:
                 datos_ASD = pd.read_csv(spectra, sep="\t", decimal=".", encoding='utf-8', on_bad_lines='skip')
@@ -78,49 +77,60 @@ class asd():
         else:
             print('Sorry, but right now we can only process ".txt" and ".asd" files')
             return None
-
+    
         if name:
             datos_ASD.columns = ["Wavelength", name]
         else:
             name = os.path.split(spectra)[1].split('.')[0]
             datos_ASD.columns = ["Wavelength", name]
 
+        # Creating the final DF and exluding values below 400 nanometers
         datos_ASD = datos_ASD[datos_ASD['Wavelength'] >= 400]
 
+        # Lists to store the data
         sat_data_cortados = []
         data_txt_cortados = []
 
+        # Filling the lists with the data 
         for banda, rango in self.sensors[self.sensor].items():
             sat_data_cortados.append(self.sat_data[(self.sat_data['SR_WL'] >= min(rango[0])) & (self.sat_data['SR_WL'] <= max(rango[0]))])
             data_txt_cortados.append(datos_ASD[(datos_ASD['Wavelength'] >= min(rango[0])) & (datos_ASD['Wavelength'] <= max(rango[0]))])
 
-        columnas_a_mantener = [[0, i] for i in range(1, len(self.sensors[self.sensor])+1)]
+        # Creating a list with the columns depending on the chosen satellite 
+        columnas_a_mantener = [[0, i] for i in range(1, len(self.sensors[self.sensor]) + 1)]
 
+        # Filling bands with values within the spectrum parts
         for i in range(len(self.sensors[self.sensor])):
             sat_data_cortados[i] = sat_data_cortados[i].iloc[:, columnas_a_mantener[i]]
 
+        # List to store the weigthed mean
         resultados_media_ponderada_sat = []
 
+        # Filling the weighthed data
         for i in range(len(self.sensors[self.sensor])):
             peso_sat = sat_data_cortados[i].iloc[:, 1]
             datos_txt = data_txt_cortados[i].iloc[:, 1]
             media_ponderada_sat = np.average(datos_txt, weights=peso_sat)
-            resultados_media_ponderada_sat.append(media_ponderada_sat)          
+            resultados_media_ponderada_sat.append(media_ponderada_sat)
 
-        fname = 'MediaPonderada'+self.sat
-        datos_sat_pond = pd.DataFrame({fname: resultados_media_ponderada_sat})        
+        # Creating the final DF
+        fname = 'MediaPonderada' + self.sat
+        datos_sat_pond = pd.DataFrame({fname: resultados_media_ponderada_sat})
         nombres_filas = [k for k, v in self.sensors[self.sensor].items()]
         datos_sat_pond.index = nombres_filas
         wavelength = [v[1] for k, v in self.sensors[self.sensor].items()]
         datos_sat_pond['Wavelength'] = wavelength
 
+        # Print band values. Not really needed since returned in the method
         if print_values:
             print(datos_sat_pond)
 
+        # Saving to csv
         if save_csv and csv_path:
             datos_sat_pond.to_csv(csv_path, index=True)
             print(f'Data saved to {csv_path}')
 
+        # Plotting
         if plot:
             plt.figure(figsize=(10, 6))
             plt.plot(datos_ASD['Wavelength'], datos_ASD[name], label=name, color='green')
@@ -133,8 +143,10 @@ class asd():
             plt.legend()
             plt.grid(True)
             plt.show()
-
+    
         return datos_sat_pond  # Return the DataFrame with expected values
+
+
 
     def plotSpecs(self, spectra_list, names=None, plot_expected=True, sats=None, print_values=False):
 
@@ -256,3 +268,105 @@ class asd():
             output_path = os.path.join(output_dir, f'{name}_all_sats.csv')
             all_sat_data_df.to_csv(output_path, index=True)
             print(f'Data saved to {output_path}')
+
+
+    def calcular_ndi(self, spec_path, b1, b2):
+        
+        """Method to calculate Normalized Differnece Index between 2 parts of the spectrums and its
+        equivalents bands in the satellites
+
+        Args:
+            spec_path (Path): Full path to the txt or asd spectrum file
+            b1 (List): List with the band 1 string name of the band/spectrum part, and lower and upper tresholds
+            b2 (List): List with the band 2 string name of the band/spectrum part, and lower and upper tresholds
+
+        Returns:
+            Dict: Dict wiht the NDI values for the spectrum and all the satellites
+        """
+
+        b1_name, b1_lower, b1_upper = b1
+        b2_name, b2_lower, b2_upper = b2
+    
+        # Dict for mapping names
+        band_name_mapping = {
+            'MSI': {
+                'Coastal blue': 'B1',
+                'Blue': 'B2',
+                'Green': 'B3',
+                'Red': 'B4',
+                'Red edge 1': 'B5',
+                'Red edge 2': 'B6',
+                'Red edge 3': 'B7',
+                'Nir': 'B8',
+                'Nir 8A': 'B8A',
+                'Water vapour': 'B9',
+                'Cirrus': 'B10',
+                'Swir 1': 'B11',
+                'Swir 2': 'B12'
+            },
+            'OLI': {
+                'Coastal blue': 'B1',
+                'Blue': 'B2',
+                'Green': 'B3',
+                'Red': 'B4',
+                'Nir': 'B5',
+                'Cirrus': 'B9',
+                'Swir 1': 'B6',
+                'Swir 2': 'B7'
+            },
+            'ETM+': {
+                'Blue': 'B1',
+                'Green': 'B2',
+                'Red': 'B3',
+                'Nir': 'B4',
+                'Swir 1': 'B5',
+                'Swir 2': 'B7'
+            },
+            'TM': {
+                'Blue': 'B1',
+                'Green': 'B2',
+                'Red': 'B3',
+                'Nir': 'B4',
+                'Swir 1': 'B5',
+                'Swir 2': 'B7'
+            }
+        }
+    
+        # Getting spectrum values
+        espectro_vals = self.spec2sat(spec_path, plot=False, print_values=False)
+    
+        # Mapping names
+        b1_real_name = band_name_mapping[self.sensor].get(b1_name)
+        b2_real_name = band_name_mapping[self.sensor].get(b2_name)
+    
+        # Obtener valores de las bandas del espectro especificadas
+        valor_b1_espectro = espectro_vals.loc[b1_real_name, 'MediaPonderada' + self.sat]
+        valor_b2_espectro = espectro_vals.loc[b2_real_name, 'MediaPonderada' + self.sat]
+    
+        # Spectrum NDI
+        ndi_espectro = (valor_b1_espectro - valor_b2_espectro) / (valor_b1_espectro + valor_b2_espectro)
+    
+        # Dict to store the sat values
+        ndi_values = {'Espectro': ndi_espectro}
+    
+        # NDI loop for each sat
+        for sat, (sensor, sheet, color) in self.sats.items():
+            self.sensor = sensor
+            self.sat_data = pd.read_excel(self.sensores, sheet_name=sheet)
+    
+            # Applying spec2sat to get values
+            sat_vals = self.spec2sat(spec_path, plot=False, print_values=False)
+        
+            # Mapping names
+            b1_real_name = band_name_mapping[sensor].get(b1_name)
+            b2_real_name = band_name_mapping[sensor].get(b2_name)
+    
+            # Getting sat bands values
+            valor_b1_sat = sat_vals.loc[b1_real_name, 'MediaPonderada' + self.sat]
+            valor_b2_sat = sat_vals.loc[b2_real_name, 'MediaPonderada' + self.sat]
+        
+            # NDI Calculation
+            ndi_sat = (valor_b1_sat - valor_b2_sat) / (valor_b1_sat + valor_b2_sat)
+            ndi_values[sat] = ndi_sat
+    
+        return ndi_values
